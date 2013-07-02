22587868e3003cb0e2c480398874a983b7260d11
Michael Leonard <maybeillrememberit@gmail.com>
2013-07-02 18:08:18 -0500

    Trying to synchronize

--- SensorCape_v0.2_clib_devel/Debug/src/test.d
+++ SensorCape_v0.2_clib_devel/Debug/src/test.d
@@ -1,3 +1,23 @@
-src/test.d: ../src/test.cpp ../src/../lib/SensorCape.h
+src/test.d: ../src/test.cpp ../src/../lib/SensorCape.h \
+ ../src/../lib/motion.h ../src/../lib/light.h ../src/../lib/am_temp.h \
+ ../src/../lib/nc_temp.h ../src/../lib/humidity.h \
+ ../src/../lib/pressure.h ../src/../lib/infrared.h \
+ ../src/../lib/utility.h
 
 ../src/../lib/SensorCape.h:
+
+../src/../lib/motion.h:
+
+../src/../lib/light.h:
+
+../src/../lib/am_temp.h:
+
+../src/../lib/nc_temp.h:
+
+../src/../lib/humidity.h:
+
+../src/../lib/pressure.h:
+
+../src/../lib/infrared.h:
+
+../src/../lib/utility.h:
