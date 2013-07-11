bea17e0010b025aa01ac692c13c31821f12adab8
Michael Leonard <maybeillrememberit@gmail.com>
2013-07-10 22:37:30 -0500

    Implemented the functions to read sensor data from MotionSensor, untested

--- SensorCape_v0.2_clib_devel/lib/motion.cpp
+++ SensorCape_v0.2_clib_devel/lib/motion.cpp
@@ -6,15 +6,59 @@
  */
  
 #include "motion.h"
+#include "i2c.h"
 #include <iostream>
 using namespace std;
 
 MotionSensor::MotionSensor() {
 	cout << "Initializing MotionSensor" << endl;
+	i2c = new I2C(2, 0xD0);
 	cout << "MotionSensor initialized" << endl;
 } 
 
 MotionSensor::~MotionSensor() {
 	cout << "Disconnecting MotionSensor" << endl;
+	delete i2c;
+	i2c = 0;
 	cout << "MotionSensor Disconnected" << endl;
+}
+
+__u32 MotionSensor::getAccelerometerX() {
+	return (i2c->read8(0x3B) << 8) | i2c->read8(0x3C);
+}
+
+__u32 MotionSensor::getAccelerometerY() {
+	return (i2c->read8(0x3D) << 8) | i2c->read8(0x3E);
+}
+
+__u32 MotionSensor::getAccelerometerZ() {
+	return (i2c->read8(0x3F) << 8) | i2c->read8(0x40);
+}
+
+__u32 MotionSensor::getGyroscopeX() {
+	return (i2c->read8(0x43) << 8) | i2c->read8(0x44);
+}
+
+__u32 MotionSensor::getGyroscopeY() {
+	return (i2c->read8(0x45) << 8) | i2c->read8(0x46);
+}
+
+__u32 MotionSensor::getGyroscopeZ() {
+	return (i2c->read8(0x47) << 8) | i2c->read8(0x48);
+}
+
+__u32 MotionSensor::getMagnetometerX() {
+	return (i2c->read8(0x04) << 8) | i2c->read8(0x03);
+}
+
+__u32 MotionSensor::getMagnetometerY() {
+	return (i2c->read8(0x06) << 8) | i2c->read8(0x05);
+}
+
+__u32 MotionSensor::getMagnetometerZ() {
+	return (i2c->read8(0x08) << 8) | i2c->read8(0x07);
+}
+
+__u32 MotionSensor::getTemp() {
+	return (i2c->read8(0x41) << 8) | i2c->read8(0x42);
 }
--- SensorCape_v0.2_clib_devel/lib/motion.h
+++ SensorCape_v0.2_clib_devel/lib/motion.h
@@ -8,11 +8,28 @@
 #ifndef MOTION_H_
 #define MOTION_H_
 
+#include "i2c.h"
+
 class MotionSensor {
 private:
+	I2C *i2c;
 public:
     MotionSensor();
     ~MotionSensor();
+
+    __u32 getAccelerometerX();
+    __u32 getAccelerometerY();
+    __u32 getAccelerometerZ();
+
+    __u32 getGyroscopeX();
+    __u32 getGyroscopeY();
+    __u32 getGyroscopeZ();
+
+    __u32 getMagnetometerX();
+    __u32 getMagnetometerY();
+    __u32 getMagnetometerZ();
+
+    __u32 getTemp();
 };
 
 #endif /* MOTION_H_ */
